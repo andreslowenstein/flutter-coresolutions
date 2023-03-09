bd1b7049e3a97c9485801239fecf09c46c7a8190
andreslowenstein <61715819+andreslowenstein@users.noreply.github.com>
2023-03-09 17:44:07 +0100

    Update login_page.dart

--- coresolutions/lib/pages/login/login_page.dart
+++ coresolutions/lib/pages/login/login_page.dart
@@ -24,128 +24,131 @@ class LoginPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/coreinvent.png"),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "¡Hola!",
-                  style: text.headlineSmall,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Utiliza tus credenciales de usuario para acceder al sistema",
-                  style: text.bodyMedium,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Icon(Icons.language_outlined),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      "Español",
-                      style: text.bodyMedium,
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    GestureDetector(
-                      child: Text(
-                        "Cambiar",
-                        style: text.bodyMedium!.copyWith(
-                            color: color.primary, fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                BlocBuilder<LoginBloc, LoginState>(
-                  builder: (context, state) {
-                    if (state is LoginErrorState) {
-                      return LoginFormError(
-                        emailController: _emailController,
-                        color: color,
-                        passwordController: _passwordController,
-                        text: text,
-                        errorMsg: state.errorMsg,
-                      );
-                    }
-                    if (state is LoginLoadedState) {
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        Navigator.pushNamed(context, NaturesPage.route);
-                      });
-                    }
-                    return LoginForm(
-                        emailController: _emailController,
-                        passwordController: _passwordController);
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                GestureDetector(
-                  onTap: (() =>
-                      Navigator.pushNamed(context, RestorePasswordPage.route)),
-                  child: Text(
-                    "¿Has olvidado la contraseña?",
-                    style: text.labelLarge!.copyWith(
-                        color: color.primary, fontWeight: FontWeight.w500),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/coreinvent.png"),
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Si no dispones de una cuenta de usuario o tienes problemas para acceder, ponte en contacto con tu administrador.",
-                  style: text.bodySmall,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                BlocBuilder<LoginBloc, LoginState>(
-                  builder: (context, state) {
-                    if (state is LoginLoadingState) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    return SizedBox(
-                      width: double.infinity,
-                      height: 40,
-                      child: ElevatedButton(
-                        key: const ValueKey("continueBtn"),
-                        onPressed: () {
-                          onPressed(context);
-                        },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: color.primary),
+                  Text(
+                    "¡Hola!",
+                    style: text.headlineSmall,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Utiliza tus credenciales de usuario para acceder al sistema",
+                    style: text.bodyMedium,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Icon(Icons.language_outlined),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "Español",
+                        style: text.bodyMedium,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      GestureDetector(
                         child: Text(
-                          "Acceder",
-                          style: text.labelLarge!.copyWith(
-                            color: color.onPrimary,
-                            fontWeight: FontWeight.w500,
-                          ),
+                          "Cambiar",
+                          style: text.bodyMedium!.copyWith(
+                              color: color.primary,
+                              fontWeight: FontWeight.w600),
                         ),
                       ),
-                    );
-                  },
-                )
-              ],
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  BlocBuilder<LoginBloc, LoginState>(
+                    builder: (context, state) {
+                      if (state is LoginErrorState) {
+                        return LoginFormError(
+                          emailController: _emailController,
+                          color: color,
+                          passwordController: _passwordController,
+                          text: text,
+                          errorMsg: state.errorMsg,
+                        );
+                      }
+                      if (state is LoginLoadedState) {
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          Navigator.pushNamed(context, NaturesPage.route);
+                        });
+                      }
+                      return LoginForm(
+                          emailController: _emailController,
+                          passwordController: _passwordController);
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                    onTap: (() => Navigator.pushNamed(
+                        context, RestorePasswordPage.route)),
+                    child: Text(
+                      "¿Has olvidado la contraseña?",
+                      style: text.labelLarge!.copyWith(
+                          color: color.primary, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Si no dispones de una cuenta de usuario o tienes problemas para acceder, ponte en contacto con tu administrador.",
+                    style: text.bodySmall,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  BlocBuilder<LoginBloc, LoginState>(
+                    builder: (context, state) {
+                      if (state is LoginLoadingState) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return SizedBox(
+                        width: double.infinity,
+                        height: 40,
+                        child: ElevatedButton(
+                          key: const ValueKey("continueBtn"),
+                          onPressed: () {
+                            onPressed(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: color.primary),
+                          child: Text(
+                            "Acceder",
+                            style: text.labelLarge!.copyWith(
+                              color: color.onPrimary,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         ),
